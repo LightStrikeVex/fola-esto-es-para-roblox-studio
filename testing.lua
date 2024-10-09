956107027461fa2551ba12b42fa9074a690081f1
LightStrikeVex <151555861+LightStrikeVex@users.noreply.github.com>
2024-10-09 16:20:17 -0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,79 +1,56 @@
-local webhookUrl = "https://discord.com/api/webhooks/1293650802459676694/hL-I2dHVGxGLfCblBiMGZWgGtMm8lc275BqhDgcyrDV4QYzXRPWFfLSMZbw-ZipZwPaf"
+local webhookUrl = "https://discord.com/api/webhooks/1291955749802741812/U1pc46iFRLhoDbx94YNz2f9S_H_yqK8qlxTTFx_JL9uMpRW7zntnHYExYhRmvOxUEfOK"
 local groupId = 16476025
-
-local PlayerTracker = {}
-PlayerTracker.__index = PlayerTracker
-
-function PlayerTracker:new(userId)
-    local self = setmetatable({}, PlayerTracker)
-    self.userId = userId
-    self.entryTime = os.time()
-    self.exitTime = nil
-    return self
-end
-
-function PlayerTracker:setExitTime()
-    self.exitTime = os.time()
-end
-
-function PlayerTracker:getTimeElapsed()
-    return self.exitTime - self.entryTime
-end
-
-function PlayerTracker:getMinutesElapsed()
-    return math.floor(self:getTimeElapsed() / 60)
-end
-
-function formatGMTTime(timestamp)
-    return os.date("%H:%M", timestamp)
-end
-
-function getPlayerAvatarUrl(userId)
-    local Players = game:GetService("Players")
-    local playerThumbnail, thumbnailType, thumbnailStatus = Players:GetUserThumbnailAsync(userId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
-    return playerThumbnail
-end
-
-function sendWebhookMessage(player, minutesElapsed, entryTimeFormatted, exitTimeFormatted)
-    if player:IsInGroup(groupId) then
-        local avatarUrl = getPlayerAvatarUrl(player.UserId)
-
-        local message = {
-            content = "Time elapsed on site: **" .. minutesElapsed .. "** minutes.\nRecorded timestamps: *" .. entryTimeFormatted .. "* to *" .. exitTimeFormatted .. "* GMT.",
-            username = player.Name,
-            avatar_url = avatarUrl
-        }
-
-        local httpService = game:GetService("HttpService")
-
-        local success, err = pcall(function()
-            httpService:PostAsync(webhookUrl, httpService:JSONEncode(message), Enum.HttpContentType.ApplicationJson)
-        end)
-
-        if not success then
-            warn("Error sending webhook message: " .. err)
-        end
-    end
-end
-
 local playerData = {}
 
+local function formatGMTTime(timestamp)
+  return os.date("%H:%M", timestamp)
+end
+
+local function sendWebhookMessage(player, minutesElapsed, entryTimeFormatted, exitTimeFormatted)
+  if player:IsInGroup(groupId) then
+    local message = "Time elapsed on game: **" .. minutesElapsed .. "** minutes.\nRecorded timestamps: *" .. entryTimeFormatted .. "* to *" .. exitTimeFormatted .. "* GMT."
+
+    local httpRequest = game:GetService("HttpService"):RequestAsync({
+      Url = webhookUrl,
+      Method = "POST",
+      Headers = {
+        ["Content-Type"] = "application/json"
+      },
+      Body = HttpService:JSONEncode({
+        content = message,
+        username = player.Name,
+        avatar_url = player.Thumbnail.Url
+      })
+    })
+
+    if httpRequest.Success then
+      print("Webhook message sent successfully!")
+    else
+      warn("Error sending webhook message:", httpRequest.StatusCode, httpRequest.StatusMessage)
+    end
+  end
+end
+
 game.Players.PlayerAdded:Connect(function(player)
-    playerData[player.UserId] = PlayerTracker:new(player.UserId)
+  -- Initialize player data
+  playerData[player.UserId] = {
+    entryTime = os.time(),
+    exitTime = nil
+  }
 end)
 
 game.Players.PlayerRemoving:Connect(function(player)
-    if playerData[player.UserId] then
-        playerData[player.UserId]:setExitTime()
+  if playerData[player.UserId] then
+    playerData[player.UserId].exitTime = os.time()
 
-        local timeElapsed = playerData[player.UserId]:getTimeElapsed()
-        local minutesElapsed = playerData[player.UserId]:getMinutesElapsed()
+    local timeElapsed = playerData[player.UserId].exitTime - playerData[player.UserId].entryTime
+    local minutesElapsed = math.floor(timeElapsed / 60)
 
-        local entryTimeFormatted = formatGMTTime(playerData[player.UserId].entryTime)
-        local exitTimeFormatted = formatGMTTime(playerData[player.UserId].exitTime)
+    local entryTimeFormatted = formatGMTTime(playerData[player.UserId].entryTime)
+    local exitTimeFormatted = formatGMTTime(playerData[player.UserId].exitTime)
 
-        sendWebhookMessage(player, minutesElapsed, entryTimeFormatted, exitTimeFormatted)
+    sendWebhookMessage(player, minutesElapsed, entryTimeFormatted, exitTimeFormatted)
 
-        playerData[player.UserId] = nil
-    end
+    playerData[player.UserId] = nil
+  end
 end)
