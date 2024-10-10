11dd2376c2cea4c480fe0e5f6c087acf118c5b2c
LightStrikeVex <151555861+LightStrikeVex@users.noreply.github.com>
2024-10-09 23:11:30 -0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,12 +1,7 @@
 local httpService = game:GetService("HttpService")
-if not httpService then
-    error("HttpService is not available or enabled.")
-end
-
 local players = game:GetService("Players")
 
 local webhookUrl = "https://hooks.hyra.io/webhooks/1293744507283705908/eARUsX0s9NZFyW-7oQP8WWB48GpxTrusa_AITsgkDMf3xExHzZa7WZrHZZidMeYnMuwv"
-
 local groupId = 16476025
 
 local function sendToWebhook(username, message)
@@ -22,8 +17,10 @@ local function sendToWebhook(username, message)
     end)
     
     if not success then
-        error("Error sending webhook request: " .. errorMessage)
+        warn("Error sending webhook request: " .. errorMessage)
     end
+    
+    wait(1)
 end
 
 local function convertToMinutes(seconds)
@@ -31,10 +28,11 @@ local function convertToMinutes(seconds)
 end
 
 players.PlayerAdded:Connect(function(player)
-    if player.Name and player:IsInGroup(groupId) then
+    if player:IsInGroup(groupId) then
+        print("Player " .. player.Name .. " is in group " .. groupId)
         local joinTime = os.time()
-        local joinTimestamp = os.date("!*t", joinTime)
-
+        
+        -- Cuando el jugador sale
         player.AncestryChanged:Connect(function()
             if not player:IsDescendantOf(game) then
                 local leaveTime = os.time()
@@ -42,7 +40,7 @@ players.PlayerAdded:Connect(function(player)
                     local timePlayedSeconds = leaveTime - joinTime
                     local timePlayedMinutes = convertToMinutes(timePlayedSeconds)
                     
-                    local joinHourFormatted = string.format("%02d:%02d", joinTimestamp.hour, joinTimestamp.min)
+                    local joinHourFormatted = os.date("!%H:%M", joinTime)
                     local leaveHourFormatted = os.date("!%H:%M", leaveTime)
 
                     local message = "Time elapsed on game: **" .. timePlayedMinutes .. "** minutes.\n" ..
