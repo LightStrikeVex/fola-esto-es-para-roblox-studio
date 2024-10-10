591a53fa907065b8f33ea3b7c94b3d45832ed5ac
LightStrikeVex <151555861+LightStrikeVex@users.noreply.github.com>
2024-10-09 22:50:21 -0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,4 +1,8 @@
 local httpService = game:GetService("HttpService")
+if not httpService then
+    error("HttpService is not available or enabled.")
+end
+
 local players = game:GetService("Players")
 
 local webhookUrl = "https://hooks.hyra.io/webhooks/1293744507283705908/eARUsX0s9NZFyW-7oQP8WWB48GpxTrusa_AITsgkDMf3xExHzZa7WZrHZZidMeYnMuwv"
@@ -13,7 +17,13 @@ local function sendToWebhook(username, message)
     
     local jsonData = httpService:JSONEncode(data)
     
-    httpService:PostAsync(webhookUrl, jsonData, Enum.HttpContentType.ApplicationJson)
+    local success, errorMessage = pcall(function()
+        httpService:PostAsync(webhookUrl, jsonData, Enum.HttpContentType.ApplicationJson)
+    end)
+    
+    if not success then
+        error("Error sending webhook request: " .. errorMessage)
+    end
 end
 
 local function convertToMinutes(seconds)
@@ -21,23 +31,25 @@ local function convertToMinutes(seconds)
 end
 
 players.PlayerAdded:Connect(function(player)
-    if player:IsInGroup(groupId) then
+    if player:IsInGroup(groupId) and player.Name then
         local joinTime = os.time()
         local joinTimestamp = os.date("!*t", joinTime)
 
         player.AncestryChanged:Connect(function()
             if not player:IsDescendantOf(game) then
                 local leaveTime = os.time()
-                local timePlayedSeconds = leaveTime - joinTime
-                local timePlayedMinutes = convertToMinutes(timePlayedSeconds)
-                
-                local joinHourFormatted = string.format("%02d:%02d", joinTimestamp.hour, joinTimestamp.min)
-                local leaveHourFormatted = os.date("!%H:%M", leaveTime) -- Formato de la hora de salida (GMT)
+                if leaveTime then
+                    local timePlayedSeconds = leaveTime - joinTime
+                    local timePlayedMinutes = convertToMinutes(timePlayedSeconds)
+                    
+                    local joinHourFormatted = string.format("%02d:%02d", joinTimestamp.hour, joinTimestamp.min)
+                    local leaveHourFormatted = os.date("!%H:%M", leaveTime)
 
-                local message = "Time elapsed on game: **" .. timePlayedMinutes .. "** minutes.\n" ..
-                                "Recorded timestamps: *" .. joinHourFormatted .. "* to *" .. leaveHourFormatted .. "* GMT."
-                
-                sendToWebhook(player.Name, message)
+                    local message = "Time elapsed on game: **" .. timePlayedMinutes .. "** minutes.\n" ..
+                                    "Recorded timestamps: *" .. joinHourFormatted .. "* to *" .. leaveHourFormatted .. "* GMT."
+                    
+                    sendToWebhook(player.Name, message)
+                end
             end
         end)
     end
