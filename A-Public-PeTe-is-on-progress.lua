50ceee721c2c89fdbf603fa6da9456a3fe52cb38
LightStrikeVex <151555861+LightStrikeVex@users.noreply.github.com>
2024-10-09 23:39:18 -0300

    Update A-Public-PeTe-is-on-progress.lua

--- A-Public-PeTe-is-on-progress.lua
+++ A-Public-PeTe-is-on-progress.lua
@@ -1,15 +1,14 @@
 local httpService = game:GetService("HttpService")
 local players = game:GetService("Players")
-local userIdToPfp = {}
-
+}
 local webhookUrl = "https://discord.com/api/webhooks/1293744507283705908/eARUsX0s9NZFyW-7oQP8WWB48GpxTrusa_AITsgkDMf3xExHzZa7WZrHZZidMeYnMuwv"
 local groupId = 16476025
-
-local function sendToWebhook(username, message, pfpUrl)
+}
+local function sendToWebhook(username, message, avatarUrl)
     local data = {
         ["username"] = username,
         ["content"] = message,
-        ["avatar_url"] = pfpUrl
+        ["avatar_url"] = avatarUrl
     }
     
     local jsonData = httpService:JSONEncode(data)
@@ -21,27 +20,12 @@ local function sendToWebhook(username, message, pfpUrl)
     if not success then
         warn("Error sending webhook request: " .. errorMessage)
     end
-    
-    wait(1)
+}
+    wait(1)}
 end
-
 local function convertToMinutes(seconds)
     return math.ceil(seconds / 60)
 end
-local function getUserPfp(userId)
-    if userIdToPfp[userId] then
-        return userIdToPfp[userId]
-    end
-    
-    local pfpUrl = "https://www.roblox.com/headshot-thumbnail/json?userId=" .. userId .. "&width=420&height=420"
-    local response = httpService:GetAsync(pfpUrl)
-    local jsonData = httpService:JSONDecode(response)
-    local pfpUrl = jsonData.Url
-    
-    userIdToPfp[userId] = pfpUrl
-    return pfpUrl
-end
-
 players.PlayerAdded:Connect(function(player)
     if player:IsInGroup(groupId) then
         print("Player " .. player.Name .. " is in group " .. groupId)
@@ -60,9 +44,9 @@ players.PlayerAdded:Connect(function(player)
                     local message = "Time elapsed on game: **" .. timePlayedMinutes .. "** minutes.\n" ..
                                     "Recorded timestamps: *" .. joinHourFormatted .. "* to *" .. leaveHourFormatted .. "* GMT."
                     
-                    local pfpUrl = getUserPfp(player.UserId)
+                    local avatarUrl = "https://www.roblox.com/headshot-thumbnail/json?userId=" .. player.UserId .. "&width=420&height=420"
                     
-                    sendToWebhook(player.Name, message, pfpUrl)
+                    sendToWebhook(player.Name, message, avatarUrl)
                 end
             end
         end)
