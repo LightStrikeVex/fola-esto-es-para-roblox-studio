afa3be998076a58a73fb015298835d6b3ff5c907
LightStrikeVex <151555861+LightStrikeVex@users.noreply.github.com>
2024-10-09 23:47:41 -0300

    Update A-Public-PeTe-is-on-progress.lua

--- A-Public-PeTe-is-on-progress.lua
+++ A-Public-PeTe-is-on-progress.lua
@@ -1,14 +1,15 @@
 local httpService = game:GetService("HttpService")
 local players = game:GetService("Players")
-}
+
 local webhookUrl = "https://discord.com/api/webhooks/1293744507283705908/eARUsX0s9NZFyW-7oQP8WWB48GpxTrusa_AITsgkDMf3xExHzZa7WZrHZZidMeYnMuwv"
 local groupId = 16476025
-}
-local function sendToWebhook(username, message, avatarUrl)
+
+local playerTimePlayed = {}
+
+local function sendToWebhook(username, message)
     local data = {
         ["username"] = username,
-        ["content"] = message,
-        ["avatar_url"] = avatarUrl
+        ["content"] = message
     }
     
     local jsonData = httpService:JSONEncode(data)
@@ -20,12 +21,14 @@ local function sendToWebhook(username, message, avatarUrl)
     if not success then
         warn("Error sending webhook request: " .. errorMessage)
     end
-}
-    wait(1)}
+    
+    wait(1)
 end
+
 local function convertToMinutes(seconds)
     return math.ceil(seconds / 60)
 end
+
 players.PlayerAdded:Connect(function(player)
     if player:IsInGroup(groupId) then
         print("Player " .. player.Name .. " is in group " .. groupId)
@@ -38,15 +41,19 @@ players.PlayerAdded:Connect(function(player)
                     local timePlayedSeconds = leaveTime - joinTime
                     local timePlayedMinutes = convertToMinutes(timePlayedSeconds)
                     
+                    if playerTimePlayed[player.UserId] then
+                        playerTimePlayed[player.UserId] = playerTimePlayed[player.UserId] + timePlayedMinutes
+                    else
+                        playerTimePlayed[player.UserId] = timePlayedMinutes
+                    end
+                    
                     local joinHourFormatted = os.date("!%H:%M", joinTime)
                     local leaveHourFormatted = os.date("!%H:%M", leaveTime)
 
-                    local message = "Time elapsed on game: **" .. timePlayedMinutes .. "** minutes.\n" ..
+                    local message = "Time elapsed on game: **" .. playerTimePlayed[player.UserId] .. "** minutes.\n" ..
                                     "Recorded timestamps: *" .. joinHourFormatted .. "* to *" .. leaveHourFormatted .. "* GMT."
                     
-                    local avatarUrl = "https://www.roblox.com/headshot-thumbnail/json?userId=" .. player.UserId .. "&width=420&height=420"
-                    
-                    sendToWebhook(player.Name, message, avatarUrl)
+                    sendToWebhook(player.Name, message)
                 end
             end
         end)
