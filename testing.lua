4009c7f666f012637a6b0235a98377cd2f45365e
LightStrikeVex <151555861+LightStrikeVex@users.noreply.github.com>
2024-10-09 22:42:02 -0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,56 +1,44 @@
-local webhookUrl = "https://discord.com/api/webhooks/1291955749802741812/U1pc46iFRLhoDbx94YNz2f9S_H_yqK8qlxTTFx_JL9uMpRW7zntnHYExYhRmvOxUEfOK"
+local httpService = game:GetService("HttpService")
+local players = game:GetService("Players")
+
+local webhookUrl = "https://hooks.hyra.io/webhooks/1293744507283705908/eARUsX0s9NZFyW-7oQP8WWB48GpxTrusa_AITsgkDMf3xExHzZa7WZrHZZidMeYnMuwv"
+
 local groupId = 16476025
-local playerData = {}
 
-local function formatGMTTime(timestamp)
-  return os.date("%H:%M", timestamp)
+local function sendToWebhook(username, message)
+    local data = {
+        ["username"] = username,
+        ["content"] = message
+    }
+    
+    local jsonData = httpService:JSONEncode(data)
+    
+    httpService:PostAsync(webhookUrl, jsonData, Enum.HttpContentType.ApplicationJson)
 end
 
-local function sendWebhookMessage(player, minutesElapsed, entryTimeFormatted, exitTimeFormatted)
-  if player:IsInGroup(groupId) then
-    local message = "Time elapsed on game: **" .. minutesElapsed .. "** minutes.\nRecorded timestamps: *" .. entryTimeFormatted .. "* to *" .. exitTimeFormatted .. "* GMT."
+local function convertToMinutes(seconds)
+    return math.ceil(seconds / 60)
+end
 
-    local httpRequest = game:GetService("HttpService"):RequestAsync({
-      Url = webhookUrl,
-      Method = "POST",
-      Headers = {
-        ["Content-Type"] = "application/json"
-      },
-      Body = HttpService:JSONEncode({
-        content = message,
-        username = player.Name,
-        avatar_url = player.Thumbnail.Url
-      })
-    })
+players.PlayerAdded:Connect(function(player)
+    if player:IsInGroup(groupId) then
+        local joinTime = os.time()
+        local joinTimestamp = os.date("!*t", joinTime)
 
-    if httpRequest.Success then
-      print("Webhook message sent successfully!")
-    else
-      warn("Error sending webhook message:", httpRequest.StatusCode, httpRequest.StatusMessage)
+        player.AncestryChanged:Connect(function()
+            if not player:IsDescendantOf(game) then
+                local leaveTime = os.time()
+                local timePlayedSeconds = leaveTime - joinTime
+                local timePlayedMinutes = convertToMinutes(timePlayedSeconds)
+                
+                local joinHourFormatted = string.format("%02d:%02d", joinTimestamp.hour, joinTimestamp.min)
+                local leaveHourFormatted = os.date("!%H:%M", leaveTime) -- Formato de la hora de salida (GMT)
+
+                local message = "Time elapsed on game: **" .. timePlayedMinutes .. "** minutes.\n" ..
+                                "Recorded timestamps: *" .. joinHourFormatted .. "* to *" .. leaveHourFormatted .. "* GMT."
+                
+                sendToWebhook(player.Name, message)
+            end
+        end)
     end
-  end
-end
-
-game.Players.PlayerAdded:Connect(function(player)
-  -- Initialize player data
-  playerData[player.UserId] = {
-    entryTime = os.time(),
-    exitTime = nil
-  }
-end)
-
-game.Players.PlayerRemoving:Connect(function(player)
-  if playerData[player.UserId] then
-    playerData[player.UserId].exitTime = os.time()
-
-    local timeElapsed = playerData[player.UserId].exitTime - playerData[player.UserId].entryTime
-    local minutesElapsed = math.floor(timeElapsed / 60)
-
-    local entryTimeFormatted = formatGMTTime(playerData[player.UserId].entryTime)
-    local exitTimeFormatted = formatGMTTime(playerData[player.UserId].exitTime)
-
-    sendWebhookMessage(player, minutesElapsed, entryTimeFormatted, exitTimeFormatted)
-
-    playerData[player.UserId] = nil
-  end
 end)
